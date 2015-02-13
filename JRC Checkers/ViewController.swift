2457fd2a25ea5c206bdaab7ba7fd650fbaaace6a
Johnny Hsiao <johnnyhsiao87@gmail.com>
2015-02-12 21:14:31 -0500

    Initial Commit

---
+++ JRC Checkers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JRC Checkers
+//
+//  Created by Shark on 2015-02-12.
+//  Copyright (c) 2015 Jollyrancher Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
