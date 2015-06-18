5ce5d3846a94702d7ec39f11b56c649d80f0382e
Johnny Hsiao <johnnyhsiao87@gmail.com>
2015-06-18 13:57:12 -0400

    Revert fb3b9bf..1de16f2
    This rolls back to commit fb3b9bf012513a9daa7021bd0a2d648d33621604.

--- JRC Checkers/ViewController.swift
+++ JRC Checkers/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,66 +14,23 @@ class ViewController: UIViewController {
     
     @IBOutlet var resetButton: UIButton!
     
-    @IBOutlet var popupMessage: UILabel!
-    
-    @IBOutlet var popupYes: UIButton!
-    
-    @IBOutlet var popupNo: UIButton!
-    
-    var audioPlayer = AVAudioPlayer()
+
     
     
     @IBAction func resetButtonClicked(sender: UIButton) {
-        showUserMsgNButtons()
-    }
-    
-    @IBAction func yesClicked(sender: UIButton) {
- //       board.resetGame()
-        hideUserMsgNButtons()
-        
-    }
-    
-    @IBAction func noClicked(sender: UIButton) {
-        hideUserMsgNButtons()
-    }
-    
-    func showUserMsgNButtons() {
-        popupMessage.hidden = false
-        popupYes.hidden = false
-        popupNo.hidden = false
-    }
-    
-    func hideUserMsgNButtons() {
-        popupMessage.hidden = true
-        popupYes.hidden = true
-        popupNo.hidden = true
+        board.resetGame()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("pieceMoveSound", ofType: "wav")!)
-        println(alertSound)
-        
-        // Removed deprecated use of AVAudioSessionDelegate protocol
-        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
-        AVAudioSession.sharedInstance().setActive(true, error: nil)
-        
-        var error:NSError?
-        audioPlayer = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
-        audioPlayer.prepareToPlay()
-        audioPlayer.play()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-   
-    
 
-        
-    
 
 }
 
