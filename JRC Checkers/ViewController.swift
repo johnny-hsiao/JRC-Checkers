e71b45c54dee80e44516cc88f025cf31de0b8f8f
Johnny Hsiao <johnnyhsiao87@gmail.com>
2015-06-18 13:40:46 -0400

    Revert 7afacc3..f069d41
    This rolls back to commit 7afacc34fcca0a4abee81869e4208f518c3eccd1.

--- JRC Checkers/ViewController.swift
+++ JRC Checkers/ViewController.swift
@@ -14,56 +14,23 @@ class ViewController: UIViewController {
     
     @IBOutlet var resetButton: UIButton!
     
-    @IBOutlet var popupMessage: UILabel!
+
     
-    @IBOutlet var popupYes: UIButton!
-    
-    @IBOutlet var popupNo: UIButton!
-    
-    @IBOutlet var undoMoveButton: UIButton!
     
     @IBAction func resetButtonClicked(sender: UIButton) {
-        showUserMsgNButtons()
-    }
-    
-    @IBAction func yesClicked(sender: UIButton) {
         board.resetGame()
-        hideUserMsgNButtons()
-        
-    }
-    
-    @IBAction func noClicked(sender: UIButton) {
-        hideUserMsgNButtons()
-    }
-    
-    func showUserMsgNButtons() {
-        popupMessage.hidden = false
-        popupYes.hidden = false
-        popupNo.hidden = false
-    }
-    
-    func hideUserMsgNButtons() {
-        popupMessage.hidden = true
-        popupYes.hidden = true
-        popupNo.hidden = true
-    }
-    
-    @IBAction func undoMove(sender: UIButton) {
-        board.undoMove()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 }
 
